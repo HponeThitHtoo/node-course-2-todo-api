3317abcf0c472bde252afe14e04ee20f93c97099
HponeThitHtoo <white.nuzzle@gmail.com>
2019-02-07 20:52:29 +0630

    Add POST /todos route and refactor mongoose

---
+++ server/server.js
@@ -0,0 +1,66 @@
+
+/* var newTodo = new Todo({
+    text: 'Cook dinner'
+});
+
+newTodo.save().then((doc) => {
+    console.log('Saved todo', doc);
+}, (e) => {
+    console.log('Unable to save todo');
+}); */
+
+/* var otherTodo = new Todo({
+    text: 'Feed the cat',
+    completed: true,
+    completedAt: 123
+});
+
+otherTodo.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 2));
+}, (e) => {
+    console.log('Unable to save', e);
+}); */
+
+// User
+// email - require it - trim it - set type - set min length of 1
+
+
+/* var newUser = new User({
+    email: 'white.nuzzle@gmail.com'
+});
+
+newUser.save().then((doc) => {
+    console.log(JSON.stringify(doc, undefined, 2));
+}, (e) => {
+    console.log('Unable to save', e);
+}); */
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res) => {
+    // console.log(JSON.stringify(req.body, undefined, 2));
+    var todo = new Todo({
+        text: req.body.text,
+        completed: req.body.completed,
+        completedAt: req.body.completedAt
+    });
+
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e); // http 400 status code error putting manually
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Started on port 3000');
+});
