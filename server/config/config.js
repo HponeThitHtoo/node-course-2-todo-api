a433eca890033003df2212576b8b64751e88f398
HponeThitHtoo <white.nuzzle@gmail.com>
2019-02-14 20:30:40 +0630

    Update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,21 @@
 var env = process.env.NODE_ENV || 'development'; // for heroku OR for testing to use test DB
 // console.log('env *****', env);
 
-if (env === 'development') {
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json'); // auto return JS Object
+    // console.log(config);
+    var evnConfig = config[env];
+
+    // console.log(Object.keys(evnConfig)); // return keys of object
+    Object.keys(evnConfig).forEach((key) => {
+        process.env[key] = evnConfig[key];
+    });
+}
+
+/* if (env === 'development') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
 } else if (env === 'test') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://127.0.0.1:27017/TodoAppTest';
-}
+} */
